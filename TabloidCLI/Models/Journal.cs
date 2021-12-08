7660eec215158b66b393aabddbdf23924fb841e9
Colin Miller <colinraymiller@gmail.com>
2021-12-08 11:05:52 -0600

    Feature/cm-?: Add Journal Display Menu, Journal Model, Journal Repository, Journal Manager

---
+++ TabloidCLI/Models/Journal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace TabloidCLI.Models
+{
+    public class Journal
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Bio { get; set; }
+        public DateTime CreateDateTime{ get;}
+
+        public Journal()
+        {
+            CreateDateTime = DateTime.Now;
+        }
+
+
+        //public override string ToString()
+        //{
+        //    return FullName;
+        //}
+    }
+}
